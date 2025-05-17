38faca14cf91196789f6cef8731b8917f7916aaf
구혁모 <gurah129@gmail.com>
2025-05-17 20:01:40 +0900

    📦 react-router-dom 패키지 추가 + App에 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/add-info" element={<AddInfoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
