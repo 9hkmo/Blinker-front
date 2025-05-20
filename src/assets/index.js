47c1c17881fc066ea63181635a71cd5a3cd747a8
9hkmo <gurah129@gmail.com>
2025-05-20 14:45:55 +0900

    🍱 헤더, 메인 로고 이미지 추가

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+// 컴포넌트에서 import { logo, menu } from '../assets' 해서 사용
+// Barrel export 패턴 import 관리
+import logo_header from "./blinker-header-logo.svg";
+import logo_title from "./blinker-title.svg";
+
+export { logo_header, logo_title };
