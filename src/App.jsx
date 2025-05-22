21c9450a8074a2466c72ec767b83a77d94b57c38
구혁모 <gurah129@gmail.com>
2025-05-22 20:04:35 +0900

    ♻️ 헤더 경로 및 태그 페이지 경로 라우터 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { AddInfoPage } from "./pages/AddInfoPage";
 import "./styles/global.scss";
 import { ResultPage } from "./pages/ResultPage";
 
-// home: 홈페이지
+// /: 홈페이지
 // game: 게임 페이지
-// report: 태그 페이지
+// test: 태그 페이지
 // permission: 권한 페이지
 // quiz: 퀴즈 들어가기전 페이지
 // quiz/:id : 퀴즈 페이지(get 방식으로 api 가져오기?)
@@ -14,8 +14,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/home" element={<HomePage />} /> */}
-        <Route path="/report" element={<AddInfoPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/test" element={<AddInfoPage />} />
         {/* <Route path="/permission" element={<PermissionPage />} />
         <Route path="/quiz" element={<QuizHomePage />} />
         <Route path="/quiz/:id" element={<QuizPage />} /> */}
