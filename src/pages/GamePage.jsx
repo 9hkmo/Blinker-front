d53969d993bf87037565b247b616c8dd2a667646
구혁모 <gurah129@gmail.com>
2025-05-28 23:27:20 +0900

    🐛 게임 이동 중 클릭 타임아웃 리턴 설정

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -147,9 +147,12 @@ export const GamePage = () => {
   const position = path[currentIndex];
 
   useEffect(() => {
-    setTimeout(() => {
+    const smallChar = setTimeout(() => {
       setCharClicked(false); // 클릭 상태 초기화
     }, 800);
+    return () => {
+      clearTimeout(smallChar); // 컴포넌트 언마운트 시 타이머 제거
+    }
   }, [position]);
 
   const enterGame = () => {
